1c47fc9c3d951cb7e3a0deb7efa038acd3a6e238
Dustin Redmond <dustinkredmond@gmail.com>
2020-03-19 11:07:44 -0400

    Formatting change

--- src/main/java/org/gserve/menu/MainMenuBarController.java
+++ src/main/java/org/gserve/menu/MainMenuBarController.java
@@ -15,26 +15,26 @@ public class MainMenuBarController extends SelectorComposer<Component> {
     public void showSettingsForm() {
         HashMap<String,Object> arg = new HashMap<>();
 
-        arg.put("infoSelected",SystemVariables.getValue("alertLevel").equalsIgnoreCase("INFO"));
-        arg.put("warningSelected",SystemVariables.getValue("alertLevel").equalsIgnoreCase("WARNING"));
-        arg.put("errorSelected",SystemVariables.getValue("alertLevel").equalsIgnoreCase("ERROR"));
-        arg.put("offSelected",SystemVariables.getValue("alertLevel").equalsIgnoreCase("OFF"));
+        arg.put("infoSelected", SystemVariables.getValue("alertLevel").equalsIgnoreCase("INFO"));
+        arg.put("warningSelected", SystemVariables.getValue("alertLevel").equalsIgnoreCase("WARNING"));
+        arg.put("errorSelected", SystemVariables.getValue("alertLevel").equalsIgnoreCase("ERROR"));
+        arg.put("offSelected", SystemVariables.getValue("alertLevel").equalsIgnoreCase("OFF"));
 
 
         arg.put("smtpUsername", SystemVariables.getValue("smtpUsername"));
         arg.put("smtpPassword", SystemVariables.getValue("smtpPassword"));
-        arg.put("smtpServer",SystemVariables.getValue("smtpServer"));
-        arg.put("smtpPort",SystemVariables.getValue("smtpPort"));
-        arg.put("alertLevel",SystemVariables.getValue("alertLevel"));
-        arg.put("alertEmail",SystemVariables.getValue("alertEmail"));
+        arg.put("smtpServer", SystemVariables.getValue("smtpServer"));
+        arg.put("smtpPort", SystemVariables.getValue("smtpPort"));
+        arg.put("alertLevel", SystemVariables.getValue("alertLevel"));
+        arg.put("alertEmail", SystemVariables.getValue("alertEmail"));
 
-        Window window = (Window) Executions.createComponents("systemSettings.zul",null,arg);
+        Window window = (Window) Executions.createComponents("systemSettings.zul", null, arg);
         window.doModal();
     }
 
     @Listen("onClick = #menuUserSettings")
     public void showUserSettingsForm(){
-        Window window = (Window) Executions.createComponents("userSettings.zul",null,null);
+        Window window = (Window) Executions.createComponents("userSettings.zul", null, null);
         window.doModal();
     }
 }
